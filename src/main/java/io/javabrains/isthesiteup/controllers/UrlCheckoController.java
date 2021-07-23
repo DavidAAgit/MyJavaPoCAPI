e83170039384e5b24e10000fa38fb649d88df459
David Cuevas miranda <855309@corpaa.aa.com>
2021-07-22 22:14:02 -0700

    have different approach

--- src/main/java/io/javabrains/isthesiteup/controllers/UrlCheckoController.java
+++ src/main/java/io/javabrains/isthesiteup/controllers/UrlCheckoController.java
@@ -12,13 +12,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.microsoft.azure.keyvault.spring.*;
-
+import com.microsoft.azure.keyvault.KeyVaultClient;
 import com.microsoft.azure.keyvault.authentication.KeyVaultCredentials;
+import com.microsoft.azure.keyvault.models.SecretBundle;
 import com.microsoft.aad.adal4j.AuthenticationContext;
 
 import com.microsoft.aad.adal4j.AuthenticationResult;
 import com.microsoft.aad.adal4j.ClientCredential;
-import com.microsoft.azure.keyvault.authentication.KeyVaultCredentials;
 
 import com.azure.identity.DefaultAzureCredentialBuilder;
 import com.azure.security.keyvault.secrets.SecretClient;
@@ -52,18 +52,20 @@ public class UrlCheckoController {
             System.out.println("javavalue2 : " + val1);
             System.out.println("strconnection :" + connectionstring);
 
-            KeyVaultClient client = new KeyVaultClient(
-                    new ClientSecretKeyVaultCredential("f759d22c-9242-41e1-b52d-777187a17eb8", "LrBEWOpC2546z11~.XZ-A38.C4~j8Gq-4H"));
-            string KEYVAULT_URL= "https://gfdqakeyvault.vault.azure.net/secrets/GFD-ConnectionString/96ae9d85e8e44dd4b9a1a79edc9b18ff";
-            // KEYVAULT_URL is the location of the keyvault to use: https://yourkeyvault.vault.azure.net
+            KeyVaultClient client = new KeyVaultClient(new ClientSecretKeyVaultCredential(
+                    "f759d22c-9242-41e1-b52d-777187a17eb8", "LrBEWOpC2546z11~.XZ-A38.C4~j8Gq-4H"));
+            String KEYVAULT_URL = "https://gfdqakeyvault.vault.azure.net/secrets/GFD-ConnectionString/96ae9d85e8e44dd4b9a1a79edc9b18ff";
+            // KEYVAULT_URL is the location of the keyvault to use:
+            // https://yourkeyvault.vault.azure.net
             // "testSecret" is the name of the secret in the key vault
-            SecretBundle secret = client.getSecret( KEYVAULT_URL, "GFD-ConnectionString" );
-            log( secret.value() );
+            Object secret = client.getSecret(KEYVAULT_URL, "GFD-ConnectionString");
+            log(client.getSecret(KEYVAULT_URL, "GFD-ConnectionString").value());
 
             int responsecategory = conn.getResponseCode() / 100;
             if (responsecategory == 2 || responsecategory == 3) {
 
-                returnMessage = " the value1 " + val1 + " value 2" + val2 + " value 2" + connectionstring + " secret: " + secret.value() ;
+                returnMessage = " the value1 " + val1 + " value 2" + val2 + " value 2" + connectionstring + " secret: "
+                        + secret.value();
 
                 returnMessage = "######" + IS_the_up + " response number => " + responsecategory
                         + "; reponse value ==> " + conn.getResponseCode() + "!!!!!#####" + returnMessage;
