c746b1b10e60df342d5422dd010d3ed24ce6b7c7
David Cuevas miranda <855309@corpaa.aa.com>
2021-07-24 11:09:43 -0700

    move file to controllers folder

--- src/main/java/io/javabrains/isthesiteup/controllers/IsTheSiteUpApplication.java
+++ src/main/java/io/javabrains/isthesiteup/controllers/IsTheSiteUpApplication.java
@@ -1,5 +1,5 @@
 // java main pack
-package io.javabrains.isthesiteup;
+package io.javabrains.isthesiteup.controllers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
