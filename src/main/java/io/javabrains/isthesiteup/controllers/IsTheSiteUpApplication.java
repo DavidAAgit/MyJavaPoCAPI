32e739362e272610746c3278fb38dcce07e80f3b
David Cuevas miranda <855309@corpaa.aa.com>
2021-08-30 22:16:40 -0700

    extended for tomcat

--- src/main/java/io/javabrains/isthesiteup/controllers/IsTheSiteUpApplication.java
+++ src/main/java/io/javabrains/isthesiteup/controllers/IsTheSiteUpApplication.java
@@ -3,14 +3,20 @@ package io.javabrains.isthesiteup.controllers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
  
 
  
 @SpringBootApplication
-public class IsTheSiteUpApplication {
+public class IsTheSiteUpApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(IsTheSiteUpApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+		return builder.sources(IsTheSiteUpApplication.class);
+	}
 }
